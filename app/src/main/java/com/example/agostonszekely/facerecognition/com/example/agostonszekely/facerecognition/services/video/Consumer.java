1c88470b9351bffe465cba16b28bd4357d38c024
Ágoston Székely <agoston.szekely@egroup.hu>
2016-11-04 19:45:45 +0100

    Mentés

--- app/src/main/java/com/example/agostonszekely/facerecognition/com/example/agostonszekely/facerecognition/services/video/Consumer.java
+++ app/src/main/java/com/example/agostonszekely/facerecognition/com/example/agostonszekely/facerecognition/services/video/Consumer.java
@@ -1,5 +1,7 @@
 package com.example.agostonszekely.facerecognition.com.example.agostonszekely.facerecognition.services.video;
 
+import com.example.agostonszekely.facerecognition.com.example.agostonszekely.facerecognition.modules.interfaces.IFaceRecognition;
+
 import java.util.concurrent.BlockingQueue;
 
 /**
@@ -8,16 +10,23 @@ import java.util.concurrent.BlockingQueue;
 
 public class Consumer implements Runnable{
 
-    private BlockingQueue<VideoFrame> queue;
+    private BlockingQueue<IFaceRecognition> queue;
 
-    public Consumer(BlockingQueue<VideoFrame> queue) {
+    public Consumer(BlockingQueue<IFaceRecognition> queue) {
         this.queue = queue;
     }
 
     @Override
     public void run() {
         while(true) {
-            //queue.take();
+            try {
+                //nem jó, it tmár az eredményt kéne kivenni
+                IFaceRecognition result = queue.take();
+
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
--- app/src/main/java/com/example/agostonszekely/facerecognition/com/example/agostonszekely/facerecognition/services/video/Producer.java
+++ app/src/main/java/com/example/agostonszekely/facerecognition/com/example/agostonszekely/facerecognition/services/video/Producer.java
@@ -1,5 +1,14 @@
 package com.example.agostonszekely.facerecognition.com.example.agostonszekely.facerecognition.services.video;
 
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.agostonszekely.facerecognition.R;
+import com.example.agostonszekely.facerecognition.com.example.agostonszekely.facerecognition.commons.services.AsyncResponse;
+import com.example.agostonszekely.facerecognition.com.example.agostonszekely.facerecognition.modules.commons.face.wrapper.FaceProperties;
+import com.example.agostonszekely.facerecognition.com.example.agostonszekely.facerecognition.modules.interfaces.IFaceRecognition;
+import com.example.agostonszekely.facerecognition.com.example.agostonszekely.facerecognition.modules.microsoftoxford.MicrosoftProjectOxford;
+
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
 
@@ -8,16 +17,33 @@ import java.util.concurrent.BlockingQueue;
  */
 
 public class Producer implements Runnable{
-    private BlockingQueue<VideoFrame> queue = null;
+    private BlockingQueue<IFaceRecognition> queue = null;
     private List<VideoFrame> frames;
 
-    public Producer(List<VideoFrame> frames, BlockingQueue<VideoFrame> queue) {
+    public Producer(List<VideoFrame> frames, BlockingQueue<IFaceRecognition> queue) {
         this.frames = frames;
         this.queue = queue;
     }
 
     @Override
     public void run(){
-        //adding frames to queue;
+        for (VideoFrame frame : frames){
+            IFaceRecognition faceRecognition;
+
+            faceRecognition = new MicrosoftProjectOxford(new AsyncResponse<List<FaceProperties>>() {
+                @Override
+                public void processFinish(List<FaceProperties> faces) {
+                    if (faces.size() >0){
+
+                    }
+                }
+            });
+
+            try {
+                queue.put(faceRecognition.getFaceRectangle(frame.getData());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
